80b1612d15f21a5d0d31165a65c65f36e29c951d
rpmjp <124282806+rpmjp@users.noreply.github.com>
2024-08-01 01:54:51 -0400

    Update main.py
    Initialization and File Import Check: Ensure that Files.paddle and Files.ball modules are correctly imported and contain the necessary paddle and ball classes.
    
    Window and Turtle Configuration: Set the window title and size if necessary.
    
    Using screen.window_width() and screen.window_height() Efficiently: Instead of calling get_screen_lengths() multiple times within the loop, store the dimensions in variables at the start of the loop.
    
    Paddle Movement Keys: Ensure the key bindings for paddle movements are correctly configured and responsive.
    
    Collision Detection: Fine-tune the collision detection for more accurate game mechanics.
    
    Exit Condition: Provide a way to exit the infinite loop.

--- main.py
+++ main.py
@@ -5,88 +5,93 @@ import time
 import random
 
 screen = turtle.Screen()
-art = turtle.Turtle()
-
-def get_screen_lengths():
-    width = screen.window_width()
-    height = screen.window_height()
-    return (width, height)
-screen.tracer(0,0)
+screen.title("Pong Game")
 screen.bgcolor('black')
+screen.setup(width=800, height=600)
+screen.tracer(0, 0)
 
-art.goto(5,-4000)
-art.lt(90)
-art.ht()
+art = turtle.Turtle()
+art.hideturtle()
+art.speed(0)
 art.color('white')
-for _ in range(800):
-    art.pd()
-    art.begin_fill()
-    for _ in range(2):
-        art.fd(100)
-        art.lt(90)
-        art.fd(10)
-        art.lt(90)
-    art.end_fill()
-    art.pu()
-    art.fd(150)
-    
-    
+
+def draw_net():
+    art.penup()
+    art.goto(0, -300)
+    art.setheading(90)
+    for _ in range(25):
+        art.pendown()
+        art.forward(10)
+        art.penup()
+        art.forward(10)
+
+draw_net()
+
 paddle = paddleModule.paddle("player")
-paddleAI = paddleModule.paddle()
+paddleAI = paddleModule.paddle("AI")
 paddleAI.direction = "up"
 ball = ballModule.ball()
+
 screen.listen()
-screen.onkeypress(paddle.go_down,key="Down")
-screen.onkeypress(paddle.go_up,key="Up")
-i=0
+screen.onkeypress(paddle.go_down, key="Down")
+screen.onkeypress(paddle.go_up, key="Up")
+
 scoreboard = turtle.Turtle()
 scoreboard.color('white')
-scoreboard.ht()
-scoreboard.pu()
+scoreboard.hideturtle()
+scoreboard.penup()
+
+i = 0
+
 while True:
     time.sleep(0.05)
+    screen_width = screen.window_width()
+    screen_height = screen.window_height()
+    
     scoreboard.clear()
-    paddle.tutel.goto(get_screen_lengths()[0]/2-50,paddle.tutel.ycor())
-    paddleAI.tutel.goto(-get_screen_lengths()[0]/2+50,paddleAI.tutel.ycor())
+    scoreboard.goto(0, screen_height / 2 - 50)
+    scoreboard.write(f"Player: {paddle.score}      AI: {paddleAI.score}", align="center", font=("Arial", 24, "normal"))
+
+    paddle.tutel.goto(screen_width / 2 - 50, paddle.tutel.ycor())
+    paddleAI.tutel.goto(-screen_width / 2 + 50, paddleAI.tutel.ycor())
+    
     ball.move()
-    paddleAI.tutel.goto(-get_screen_lengths()[0]/2+50,ball.tutel.ycor())
-    # if paddleAI.direction == "up":
-    #     paddleAI.go_up()
-    # else:
-    #     paddleAI.go_down()
-    if paddle.tutel.ycor() > get_screen_lengths()[1]/2+75:
-        paddle.tutel.goto(paddle.tutel.xcor(),-get_screen_lengths()[1]/2-75)
-    elif paddle.tutel.ycor() < -get_screen_lengths()[1]/2-75:
-        paddle.tutel.goto(paddle.tutel.xcor(),get_screen_lengths()[1]/2+75)
-    if paddleAI.tutel.ycor() > get_screen_lengths()[1]/2+75:
-        paddleAI.tutel.goto(paddleAI.tutel.xcor(),-get_screen_lengths()[1]/2-75)
-    elif paddleAI.tutel.ycor() < -get_screen_lengths()[1]/2-75:
-        paddleAI.tutel.goto(paddleAI.tutel.xcor(),get_screen_lengths()[1]/2+75)
+    paddleAI.tutel.goto(-screen_width / 2 + 50, ball.tutel.ycor())
+    
+    if paddle.tutel.ycor() > screen_height / 2 - 50:
+        paddle.tutel.sety(screen_height / 2 - 50)
+    elif paddle.tutel.ycor() < -screen_height / 2 + 50:
+        paddle.tutel.sety(-screen_height / 2 + 50)
+    
+    if paddleAI.tutel.ycor() > screen_height / 2 - 50:
+        paddleAI.tutel.sety(screen_height / 2 - 50)
+    elif paddleAI.tutel.ycor() < -screen_height / 2 + 50:
+        paddleAI.tutel.sety(-screen_height / 2 + 50)
     
     if i == 100:
-        i=0
-        paddleAI.direction = random.choice(("up","down"))
+        i = 0
+        paddleAI.direction = random.choice(("up", "down"))
     else:
-        i+=1
+        i += 1
         
-    if ball.tutel.ycor() > get_screen_lengths()[1]/2-10 or ball.tutel.ycor() < -get_screen_lengths()[1]/2+10:
+    if ball.tutel.ycor() > screen_height / 2 - 10 or ball.tutel.ycor() < -screen_height / 2 + 10:
         ball.yspeed *= -1
-    if ball.tutel.xcor() > get_screen_lengths()[0]/2-10 or ball.tutel.xcor() < -get_screen_lengths()[0]/2+10:
+    if ball.tutel.xcor() > screen_width / 2 - 10 or ball.tutel.xcor() < -screen_width / 2 + 10:
         ball.xspeed *= -1
-        if ball.tutel.xcor() > get_screen_lengths()[0]/2-10:
+        if ball.tutel.xcor() > screen_width / 2 - 10:
             paddleAI.score += 1
         else:
             paddle.score += 1
         
-    if ball.tutel.xcor() > get_screen_lengths()[0]/2-70 and ball.tutel.ycor() <= paddle.tutel.ycor()+50 and ball.tutel.ycor() >= paddle.tutel.ycor()-50:
+    if ball.tutel.xcor() > screen_width / 2 - 70 and paddle.tutel.ycor() - 50 < ball.tutel.ycor() < paddle.tutel.ycor() + 50:
         ball.xspeed *= -1
-    if ball.tutel.xcor() < (get_screen_lengths()[0]*-1)/2+70 and ball.tutel.ycor() <= paddleAI.tutel.ycor()+50 and ball.tutel.ycor() >= paddleAI.tutel.ycor()-50:
+    if ball.tutel.xcor() < -screen_width / 2 + 70 and paddleAI.tutel.ycor() - 50 < ball.tutel.ycor() < paddleAI.tutel.ycor() + 50:
         ball.xspeed *= -1
 
-    scoreboard.write(f"Player: {paddle.score}      AI: {paddleAI.score}",align="right",font=("Arial",24,"normal"))
-    scoreboard.goto(0,get_screen_lengths()[1]/2-50)
     screen.update()
+    
 screen.exitonclick()
+
 
 
 # TODO add collision detection with ball and paddles
